69e7e1d9eec999ee9486f6469d16f1524e2cdb29
Kim Marquardt <kim.marquardt@gmail.com>
2014-09-22 15:11:17 -0700

    Add checkin schema

--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ var Friend = new Schema({
   latitude: String,
   longitude: String,
   picture_url: String
-})
+});
 
 var User = new Schema({
   first_name: String,
@@ -15,7 +15,23 @@ var User = new Schema({
   latitude: String,
   longitude: String,
   fbId: Number
-})
+});
+
+var Checkin = new Schema({
+  // id to checkin object and not the place
+  fbId: Number,
+  created_at: String
+  // the place
+  place: {
+    fbId: Number,
+    name: String,
+    photo: String
+  },
+  latitude: String,
+  longitude: String,
+  // the personal
+  from: { name: String, fbId: Number }
+});
 
 exports.friendSchema = mongoose.model('friend', Friend);
 exports.userSchema = mongoose.model('user', User);
