0f01aa6ab0115105cfa5dda10e757ee4a609cec3
Kim Marquardt <kim.marquardt@gmail.com>
2014-10-07 09:56:55 -0700

    Wrap client_rendering.js in a function

--- public/js/backbone/AppView.js
+++ public/js/backbone/AppView.js
@@ -1,57 +1,48 @@
-// var loginModel = new (Backbone.Model.extend({
+var loginModel = new (Backbone.Model.extend({
+  initialize: function(){
+    this.set('loginStatus', 'unknown');
+    this.set('rendered', false);
+  }
+}));
 
-//   initialize: function(){
-//     this.set('loginStatus', 'unknown');
-//   }
-// }));
-// check Facebook login status every once and a while to check which view to display
-function keepTabsOnLoginState(){
-  setInterval(function(){
-    if(FB){
-      FB.getLoginStatus(function (response){
-        console.log('status check callback:', response.status);
-        // var currentStatus = appViewContext.model.get('loginStatus');
-        var newStatus = response.status;
-        // if(newStatus !== currentStatus){
-        //   appViewContext.model.set('loginStatus', newStatus);
-        // }
-        if(newStatus === 'connected'){
-          console.log('the view is globeView');
-          appViewContext.render('globeView');
-        }
-        if(newStatus !== 'connected'){
-          console.log('the view is loginView');
-          appViewContext.render('loginView');
-        }
-      });
-    }
-  }, 100);
-}
-
-
-// var sphere_script = document.createElement('script');
-// sphere_script.src = 'js/sphere_graph.js';
-// document.body.appendChild(sphere_script);
 
 var appView = new (Backbone.View.extend({
-  // model: loginModel,
+
+  model: loginModel,
 
   initialize: function(){
-    var appViewContext = this;
+    this.keepTabsOnLoginState();
+  },
 
-    keepTabsOnLoginState();
+  // check Facebook login status every once and a while to check which view to display
+  keepTabsOnLoginState: function (){
+    var appViewContext = this;
+    setInterval(function(){
+      if(FB){
+        FB.getLoginStatus(function (response){
+          // var currentStatus = appViewContext.model.get('loginStatus');
+          var newStatus = response.status;
+          // if(newStatus !== currentStatus){
+          //   appViewContext.model.set('loginStatus', newStatus);
+          // }
+          if(newStatus === 'connected'){
+            appViewContext.render('globeView');
+          }
+          if(newStatus !== 'connected'){
+            appViewContext.render('loginView');
+          }
+        });
+      }
+    }, 100);
   },
 
   render: function(view){
     console.log('in render');
     if(view === 'globeView'){
-      console.log('render', view);
-      var sphere_script = document.createElement('script');
-      var rendering_script = document.createElement('script');
-      sphere_script.src = 'js/sphere_graph.js';
-      rendering_script.src = 'js/client_rendering.js';
-      document.body.appendChild(sphere_script);
-      document.body.appendChild(rendering_script);
+      if(this.model.get('rendered') === false){
+        initialize3d();
+        this.model.set('rendered', true);
+      }
     } else if(view === 'loginView'){
       console.log('render', view);
       $('body').html('scoobydoo');
--- public/js/client_rendering.js
+++ public/js/client_rendering.js
@@ -1,262 +1,264 @@
-var drawing = new Drawing.SphereGraph({numNodes: 50, showStats: true, showInfo: true});
+var initialize3d = function(){
+  var drawing = new Drawing.SphereGraph({numNodes: 50, showStats: true, showInfo: true});
 
-  $.get('/api/get-user').then(function(response){
-    var user = JSON.parse(response);
-    friendsList = user.friends;
-    userNode = drawing.createGraph(user, true);
-  })
-
-	$.get('/api/get-friends').then(function(response){
-		var friends = JSON.parse(response);
-		friends = friends.data;
-		setInterval(function(){
-			if(friends.length){
-				drawing.createGraph(friends.pop());
-			}
-		}, 0)
-	});
-
-  var nextFunc;
-  flyToNext(function(next){
-    nextFunc = next;
-  });
-
-
-  $('.mutual').on('click', function(){
-    $.get('/api/get-user').then(function(response){
-      friendList = JSON.parse(response).friends;
-      getMutual(friendList);
-    })
-  });
-
-  $('.connect').on('click', function(){
     $.get('/api/get-user').then(function(response){
       var user = JSON.parse(response);
-      drawing.addUser(user, true);
+      friendsList = user.friends;
+      userNode = drawing.createGraph(user, true);
     })
-  });
 
-  $(document).on('keydown', function( event ){
-    if(event.which === 32){ // space key
-      nextFunc();
-    }
-    else if(event.which === 13){ // enter key
-      getAllPhotos();
-    }
-    else if(event.which === 87){ // w key
-      var current = window.currentId
-      FBData.get('newsFeed', current, function(data){
-        var myPosts = JSON.parse(data);
-        if(myPosts.posts){
-          myPosts = myPosts.posts.data;
-          investigatePosts(current, myPosts);
-        }
+  	$.get('/api/get-friends').then(function(response){
+  		var friends = JSON.parse(response);
+  		friends = friends.data;
+  		setInterval(function(){
+  			if(friends.length){
+  				drawing.createGraph(friends.pop());
+  			}
+  		}, 0)
+  	});
+
+    var nextFunc;
+    flyToNext(function(next){
+      nextFunc = next;
+    });
+
+
+    $('.mutual').on('click', function(){
+      $.get('/api/get-user').then(function(response){
+        friendList = JSON.parse(response).friends;
+        getMutual(friendList);
       })
-    }
-  })
+    });
 
-  $('.newsFeed').on('click', function(){
-    FBData.get('newsFeed', 'me', function(data){
-      data = JSON.parse(data);
-      console.log(data);
-    })
-  })
-///// for info display //////////////////////////////////////////////////
-  var infoHTMLlog = [];
-  var $infoHTML = $('<div><div class="info-data img-box"></div></div>');
+    $('.connect').on('click', function(){
+      $.get('/api/get-user').then(function(response){
+        var user = JSON.parse(response);
+        drawing.addUser(user, true);
+      })
+    });
 
-  function displayInfo(data, isUrl){
-    var key;
-    if(isUrl){
-      key = data.url;
-    } else {
-      key = data.source;
-    }
-    var $infoHTMLClone = $infoHTML.clone();
-    var $info = $infoHTMLClone.find('.info-data');
-
-    //var header = $infoHTMLClone.find('.info-header');
-    if($('.panel-wrapper').children().length){
-      $($('.panel-wrapper').children()[0]).addClass('zoomOut');
-    }
-    $('.panel-wrapper').empty();
-    var image = new Image();
-    image.style.opacity = 0.6;
-    image.onload = function(){
-      var $image = $(image);
-      $image.addClass('info-img animated zoomIn');
-      $info.append($image);
-      $('.panel-wrapper').append($infoHTMLClone);
-      infoHTMLlog.push($infoHTMLClone);
-    }
-    image.src = key;
-  }
-//////////////////////////////////////////////////////////////////////////
-function flyToNext(cb){
-  $.get('/api/get-user').then(function(response){
-      var friends = JSON.parse(response).friends;
-      var last;
-      cb(function(){ 
-        var i = Math.floor(Math.random()*friends.length);
-        current = friends[i];
-        currentNode = drawing.getNode(current);
-        while(!currentNode || current === last){
-          i += 1;
-          if(i === friends.length){
-            i = 0;
+    $(document).on('keydown', function( event ){
+      if(event.which === 32){ // space key
+        nextFunc();
+      }
+      else if(event.which === 13){ // enter key
+        getAllPhotos();
+      }
+      else if(event.which === 87){ // w key
+        var current = window.currentId
+        FBData.get('newsFeed', current, function(data){
+          var myPosts = JSON.parse(data);
+          if(myPosts.posts){
+            myPosts = myPosts.posts.data;
+            investigatePosts(current, myPosts);
           }
-          current = friends[i];
-          currentNode = drawing.getNode(current);
-        }
-        //go to next user on globe and draw mutual friends
-        window.currentId = current
-        drawing.goToNode(current);
-        last = current;
-        getMutual(current);
-        getPic(current);
-      })
-  })
-}
-
-var getAllPhotos = function(id){
-  id = id || window.currentId;
-  if(current === undefined){
-    var current = window.currentId;
-  }
-  FBData.get('userPhotos',current, function(data){
-    //get photos of current friend
-    data = JSON.parse(data);
-    console.log('data: ', data)
-    if(data.photos){
-      //if there are photos, display them
-      getPhotos(data.photos.data, id);
-    }
-  })
-}
-
-var batchPhotos = function(idArray){
-  var dataArray = [];
-  if(idArray.length){
-    idArray.forEach(function(id){
-      dataArray.push({method: 'GET', relative_url: '/'+id});
-    })
-  }
-  FBData.batch(dataArray, function(data){
-    console.log(data);
-  })
-}
-
-var getPhotos = function(array, id){
-  var friend = drawing.getNode(id);
-  //every second, get a photo from FB and display
-  if(array.length){
-    setInterval(function(){
-      if(array.length){
-        var current = array.pop();
-        FBData.get('getPhoto', current.id, function(photoData){
-          photoData = JSON.parse(photoData);
-          drawing.displayPhoto(photoData, friend);
         })
       }
-    }, 20)
+    })
+
+    $('.newsFeed').on('click', function(){
+      FBData.get('newsFeed', 'me', function(data){
+        data = JSON.parse(data);
+        console.log(data);
+      })
+    })
+  ///// for info display //////////////////////////////////////////////////
+    var infoHTMLlog = [];
+    var $infoHTML = $('<div><div class="info-data img-box"></div></div>');
+
+    function displayInfo(data, isUrl){
+      var key;
+      if(isUrl){
+        key = data.url;
+      } else {
+        key = data.source;
+      }
+      var $infoHTMLClone = $infoHTML.clone();
+      var $info = $infoHTMLClone.find('.info-data');
+
+      //var header = $infoHTMLClone.find('.info-header');
+      if($('.panel-wrapper').children().length){
+        $($('.panel-wrapper').children()[0]).addClass('zoomOut');
+      }
+      $('.panel-wrapper').empty();
+      var image = new Image();
+      image.style.opacity = 0.6;
+      image.onload = function(){
+        var $image = $(image);
+        $image.addClass('info-img animated zoomIn');
+        $info.append($image);
+        $('.panel-wrapper').append($infoHTMLClone);
+        infoHTMLlog.push($infoHTMLClone);
+      }
+      image.src = key;
+    }
+  //////////////////////////////////////////////////////////////////////////
+  function flyToNext(cb){
+    $.get('/api/get-user').then(function(response){
+        var friends = JSON.parse(response).friends;
+        var last;
+        cb(function(){
+          var i = Math.floor(Math.random()*friends.length);
+          current = friends[i];
+          currentNode = drawing.getNode(current);
+          while(!currentNode || current === last){
+            i += 1;
+            if(i === friends.length){
+              i = 0;
+            }
+            current = friends[i];
+            currentNode = drawing.getNode(current);
+          }
+          //go to next user on globe and draw mutual friends
+          window.currentId = current
+          drawing.goToNode(current);
+          last = current;
+          getMutual(current);
+          getPic(current);
+        })
+    })
+  }
+
+  var getAllPhotos = function(id){
+    id = id || window.currentId;
+    if(current === undefined){
+      var current = window.currentId;
+    }
+    FBData.get('userPhotos',current, function(data){
+      //get photos of current friend
+      data = JSON.parse(data);
+      console.log('data: ', data)
+      if(data.photos){
+        //if there are photos, display them
+        getPhotos(data.photos.data, id);
+      }
+    })
+  }
+
+  var batchPhotos = function(idArray){
+    var dataArray = [];
+    if(idArray.length){
+      idArray.forEach(function(id){
+        dataArray.push({method: 'GET', relative_url: '/'+id});
+      })
+    }
+    FBData.batch(dataArray, function(data){
+      console.log(data);
+    })
+  }
+
+  var getPhotos = function(array, id){
+    var friend = drawing.getNode(id);
+    //every second, get a photo from FB and display
+    if(array.length){
+      setInterval(function(){
+        if(array.length){
+          var current = array.pop();
+          FBData.get('getPhoto', current.id, function(photoData){
+            photoData = JSON.parse(photoData);
+            drawing.displayPhoto(photoData, friend);
+          })
+        }
+      }, 20)
+    }
+  };
+
+  window.getProfilePic = function(id){
+    if(id === id){
+      getPic(id);
+    }
+  };
+
+  var getPic = function(id){
+    FBData.get('getProfilePic', id, function(photo){
+      photo = JSON.parse(photo);
+      photo = photo.picture.data;
+      displayInfo(photo, true);
+    })
+  }
+
+  function goToRelay(id){
+    if(id === id){
+      window.currentId = id;
+    }
+    if(drawing !== undefined){
+      drawing.goToNode(id);
+    }
+  }
+
+  function getMutual (idArray, connectUser){
+    if(connectUser && drawing !== undefined){
+      var node = drawing.getNode(idArray);
+      if(node==undefined){
+        console.log("node not found");
+      } else {
+        drawing.connectToUser(node);
+      }
+    }
+    if(Array.isArray(idArray)){
+      var currentFriend = idArray.pop();
+    } else {
+      var currentFriend = idArray;
+      idArray = [];
+    }
+    var payload = {id: currentFriend};
+    $.post('/api/get-mutual', payload).then(function(response){
+      var mutualList = JSON.parse(response);
+      if(mutualList.length){
+        loadMutual(mutualList, currentFriend);
+      }
+    })
+    if(idArray.length){
+        return getMutual(idArray);
+    } else {
+        return;
+    }
+  }
+
+  function loadMutual(list, currentFriend){
+    var currentMutual = list.pop();
+    drawing.addEdge(currentFriend, currentMutual, 'red', true);
+    if(list.length){
+      return loadMutual(list, currentFriend);
+    } else {
+      return;
+    }
+  }
+
+  var investigatePosts = function(id, posts){
+    drawing.moveOut();
+    if(!posts || !posts.length){
+      return;
+    } else {
+      if(posts.length > 12){
+        posts = posts.slice(0,12);
+      }
+      setInterval(function(){
+        drawPosts(id, posts);
+      }, 300)
+    }
+  }
+
+  var drawPosts = function(id, posts){
+    var current = posts.pop();
+    current = drawing.addPost(id, current, drawing);
+    FBData.getPostLikes(current.id, function(data){
+      data = data.data;
+      for(var l = 0; l < data.length; l++){
+        var liker = data[l];
+        liker = drawing.getNode(liker.id);
+        if(drawing.getNode(liker.id) !== undefined){
+          drawing.addEdge(current.id, liker.id, 'green', true);
+        }
+      }
+      if(posts.length){
+        return drawPosts(id, posts);
+      } else {
+        return;
+      }
+    })
   }
 };
-
-window.getProfilePic = function(id){
-  if(id === id){
-    getPic(id);  
-  }
-};
-
-var getPic = function(id){
-  FBData.get('getProfilePic', id, function(photo){
-    photo = JSON.parse(photo);
-    photo = photo.picture.data;
-    displayInfo(photo, true);
-  })
-}
-
-function goToRelay(id){
-  if(id === id){
-    window.currentId = id;
-  }
-  if(drawing !== undefined){
-    drawing.goToNode(id);
-  }
-}
-
-function getMutual (idArray, connectUser){
-  if(connectUser && drawing !== undefined){
-    var node = drawing.getNode(idArray);
-    if(node==undefined){
-      console.log("node not found");
-    } else {
-      drawing.connectToUser(node);
-    }
-  }
-  if(Array.isArray(idArray)){
-    var currentFriend = idArray.pop();  
-  } else {
-    var currentFriend = idArray;
-    idArray = [];
-  }
-  var payload = {id: currentFriend};
-  $.post('/api/get-mutual', payload).then(function(response){
-    var mutualList = JSON.parse(response);
-    if(mutualList.length){
-      loadMutual(mutualList, currentFriend);
-    }
-  })
-  if(idArray.length){
-      return getMutual(idArray);
-  } else {
-      return;
-  }
-}
-
-function loadMutual(list, currentFriend){
-  var currentMutual = list.pop();
-  drawing.addEdge(currentFriend, currentMutual, 'red', true);
-  if(list.length){
-    return loadMutual(list, currentFriend);
-  } else {
-    return;
-  }
-}    
-
-var investigatePosts = function(id, posts){
-  drawing.moveOut();
-  if(!posts || !posts.length){
-    return;
-  } else {
-    if(posts.length > 12){
-      posts = posts.slice(0,12);
-    }
-    setInterval(function(){
-      drawPosts(id, posts);
-    }, 300)
-  }
-}
-
-var drawPosts = function(id, posts){
-  var current = posts.pop();
-  current = drawing.addPost(id, current, drawing);
-  FBData.getPostLikes(current.id, function(data){
-    data = data.data;
-    for(var l = 0; l < data.length; l++){
-      var liker = data[l];
-      liker = drawing.getNode(liker.id);
-      if(drawing.getNode(liker.id) !== undefined){
-        drawing.addEdge(current.id, liker.id, 'green', true);
-      } 
-    }
-    if(posts.length){
-      return drawPosts(id, posts);
-    } else {
-      return;
-    }
-  })
-}
   // var $infoHTML = $('<div class="panel panel-default info-box"><div class="panel-heading info-header"></div><div class="panel-body info-data"></div></div>');
   // var displayInfo = function(data){
   //   var $infoHTMLClone = $infoHTML.clone();
